bae83f4513ced774b6231d188476df84b6fc7d2e
hossam <hossamaldeenabdalah@gmail.com>
2026-03-21 13:56:27 +0200

    adding department Entity

--- src/main/java/com/empolyee/demo/entites/Department.java
+++ src/main/java/com/empolyee/demo/entites/Department.java
@@ -1,10 +1,21 @@
 package com.empolyee.demo.entites;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.annotations.UuidGenerator;
 
 import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 
 public class Department {
     @Column(name = "name", nullable = false)
